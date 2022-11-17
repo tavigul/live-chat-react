4abe653f90e79d54009f26de42ce4fbfffb45e99
menashe_ta <menashe_ta@groupbwt.com>
2022-11-17 11:56:53 +0600

    (feat): add chat interface

--- server/index.js
+++ server/index.js
@@ -17,6 +17,16 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log(`User connected: ${socket.id}`)
 
+  socket.on("join_room", (data) => {
+    socket.join(data)
+    console.log(`User with id ${socket.id} joined room ${data}`)
+  })
+
+  socket.on("send_message", (data) => {
+    console.log("data", data)
+    socket.to(data.room).emit("receive_message", data);
+  })
+
   socket.on("disconnect", () => {
     console.log("User disconnected", socket.id)
   })
